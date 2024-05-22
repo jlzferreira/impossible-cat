377ba18900a2f2105ca9ed27ca76619c77250164
José Luiz <jlz.dev@outlook.com>
2024-05-21 22:36:26 -0300

    new site

--- js/script.js
+++ js/script.js
@@ -1,9 +1,35 @@
-// window.addEventListener("DOMContentLoaded", event => {
-//     const audio = document.querySelector("audio");
-//     audio.volume = 0.2;
-//     audio.play();
+function movePage(e) {
+
+    if(e == "mid"){
+        window.location.href="#mid-page";
+    }else if(e == "miss"){
+        window.location.href="#miss";
+    }
+    
+
+    }
+
+    var source = "https://drive.google.com/file/d/15nVZ1NRJM7jhg_0f5QWbSmk6V6quxW5P/view?usp=drive_link"
+    var audio = document.createElement("audio");
+    //
+    audio.autoplay = true;
+    //
+    audio.load()
+    audio.addEventListener("load", function() { 
+        audio.play(); 
+    }, true);
+    audio.src = source;
 
     
-//   });
-
-  
+    function copyCA() {
+        // Get the text field
+        var copyText = document.getElementById("ca");
+      
+        // Select the text field
+        copyText.select();
+        copyText.setSelectionRange(0, 99999); // For mobile devices
+      
+         // Copy the text inside the text field
+        navigator.clipboard.writeText(copyText.value);
+      
+      }
